71c622e632f9b7e3df28182666dd071ee1b97e4a
iabad1 <isiahianabad@gmail.com>
2024-06-20 01:39:45 -0700

    Creating Boba Order Class and Helper Methods

--- Monke The Boba-Rista/Assets/BobaOrderDisplay.cs
+++ Monke The Boba-Rista/Assets/BobaOrderDisplay.cs
@@ -11,15 +11,10 @@ public class BobaOrderDisplay : MonoBehaviour
 
     public TextMeshProUGUI bobaOrderDisplay;
     public TextMeshProUGUI timerText;
+    public TextMeshProUGUI bobaCountText;
     private float timer = 0.0f;
     private float timeDisplayTimer = 0.0f;
-
     private float waitTime = 2.0f;
-
-    private string[] fruits = { "Mango", "Strawberry" };
-    private string[] teaBase = { "Black", "Green", "Oolong" };
-    private string[] creamer = { "Tea", "Milk Tea" };
-
 
     // Start is called before the first frame update
 
@@ -29,58 +24,88 @@ public class BobaOrderDisplay : MonoBehaviour
         InvokeRepeating("GenerateBobaOrder", 5.0f, 2.0f);
     }
     */
+    public List<BobaOrder> bobaOrders = new List<BobaOrder>();
+
+    public class BobaOrder
+    {
+        private string[] fruits = { "Mango", "Strawberry" };
+        private string[] teaBase = { "Black", "Green", "Oolong" };
+        private string[] milk = { "Tea", "Milk Tea" };
+
+        public string fruitChoice;
+        public string teaBaseChoice;
+        public string milkChoice;
+
+        public BobaOrder()
+        {
+            fruitChoice = generateFruitChoice();
+            teaBaseChoice = generateTeaBaseChoice();
+            milkChoice = generateMilkChoice();
+
+        }
+
+        private string generateFruitChoice()
+        {
+            //creating a correct order -- avoiding -1 for both fruits and teaBase
+            int fruitsChoice = Random.Range(-1, fruits.Length);
+            if (fruitsChoice != -1)
+            {
+                return fruits[fruitsChoice];
+            }
+            else
+            {
+                return "";
+            }
+
+        }
+        private string generateTeaBaseChoice()
+        {
+            int teaBaseChoice = Random.Range(0, teaBase.Length);
+            return teaBase[teaBaseChoice];
+
+        }
+        private string generateMilkChoice()
+        {
+            int milkChoice = Random.Range(0, milk.Length);
+            return milk[milkChoice];
+        }
+
+        public string displayOrder()
+        {
+            return fruitChoice + " " + teaBaseChoice + " " + milkChoice;
+        }
+
+
+    }
 
     void Update()
     {
-        
+        //updating time
         timer += Time.deltaTime;
         timeDisplayTimer += Time.deltaTime;
 
+        //per second
         int timerInSec = Mathf.FloorToInt(timeDisplayTimer % 60);
         timerText.text = timerInSec.ToString();
 
-        
+        //creating order every 2 seconds
         if (timer > waitTime)
         {
-            //Debug.Log(timr);
             Debug.Log("2 seconds");
 
-
             //generate a new boba order
-            string bobaOrder = GenerateBobaOrder();
-            bobaOrderDisplay.text += "\n" + bobaOrder;
+            BobaOrder newOrder = new BobaOrder();
+            bobaOrders.Add(newOrder);
+
+            //boba counter and boba UI display updated
+            bobaCountText.text = "BobaCount: " + bobaOrders.Count.ToString();
+            bobaOrderDisplay.text += "\n" + newOrder.displayOrder();
+
+            //reset timer 
             timer = timer - waitTime;
         }
         
 
-
     }
-
-    private string GenerateBobaOrder()
-    {
-
-
-        string finalOrder;
-        string fruitText = "";
-        string teaBaseText = "";
-        string creamerText = "";
-
-        //creating a correct order -- avoiding -1 for both fruits and teaBase
-        int fruitsChoice = Random.Range(-1, fruits.Length);
-        int teaBaseChoice = Random.Range(0, teaBase.Length);
-        int creamerChoice = Random.Range(0, creamer.Length);
-
-        if (fruitsChoice != -1)
-        {
-            fruitText = fruits[fruitsChoice];
-        }
-
-        teaBaseText = teaBase[teaBaseChoice];
-        creamerText = creamer[creamerChoice];
-
-        finalOrder = fruitText + " " + teaBaseText + " " + creamerText;
-        //bobaOrderDisplay.text += "\n" + finalOrder;
-        return finalOrder;
-    }
-
+   
 }
